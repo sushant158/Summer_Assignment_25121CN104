5c324f69e604b25f9b2054f003275bcc2469b6d7
Sushant kumar <25121cn104@glbitm.ac.in>
2026-06-01 20:42:04 +0530

    Add factorial calculation program in Day1.3.c

---
+++ Day1.3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int fact(int X);
+int main()
+{
+    int num;
+    printf("Enter the number for factorial calculation\n");
+    scanf("%d", &num);
+    printf("Factorial is %d", fact(num));
+}
+int fact(int num)
+{
+    if (num == 0 || num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * fact(num - 1);
+    }
+}
