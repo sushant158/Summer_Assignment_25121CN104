2d00b2be790ce63d5a086632054297f56d95b656
Sushant kumar <25121cn104@glbitm.ac.in>
2026-06-10 20:29:08 +0530

    Add right-aligned triangle pattern program
    Implement a program to print a right-aligned triangle pattern of asterisks based on user input for the number of rows.

---
+++ Day10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int rows;
+    printf("Enter the number of rows");
+    scanf("%d",&rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows + i - 1; j++) {
+        if (j <= rows - i){
+                printf(" ");
+            }
+            else{
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
