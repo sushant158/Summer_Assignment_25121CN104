e5fddd0e47ac84ef772f594f4a1a8a57fe0e2bb1
Sushant kumar <25121cn104@glbitm.ac.in>
2026-06-13 20:35:53 +0530

    Implement program to find largest and smallest elements

---
+++ Day13.3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int arr[50]={},num;
+    printf("Enter the number of elements\n");
+    scanf("%d",&num);
+    printf("Enter the elements\n");
+    for (int i = 0; i < num; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int largest=arr[0];
+    int smallest=arr[0];
+    for (int i = i; i < num; i++)
+    {
+        if(largest<arr[i]){
+            largest=arr[i];
+        }
+        else if(smallest>arr[i]){
+            smallest=arr[i];
+        }
+    }
+    printf("The largest element is %d and the smallest element is %d",largest,smallest);
+    
+}
