47b4920a2e12efed2d1629732a283f6e5d2d7179
Sushant kumar <25121cn104@glbitm.ac.in>
2026-06-17 20:52:21 +0530

    Add program to merge two arrays into one

---
+++ Day17.1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int arr1[5]={1,2,3,4,5};
+    int arr2[5]={6,7,8,9,10};
+    int arr3[10];
+    for (int i = 0; i < 5; i++)
+    {
+        arr3[i]=arr1[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        arr3[i+5]=arr2[i];
+    }
+    printf("The merged array is \n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ",arr3[i]);
+    }
+    
+    
+    
+    
+}
