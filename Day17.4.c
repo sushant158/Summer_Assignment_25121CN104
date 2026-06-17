d979b37748d33ea590b80e17b2a71cc3c94a6ac2
Sushant kumar <25121cn104@glbitm.ac.in>
2026-06-17 21:15:04 +0530

    Add program to find common elements in two arrays

---
+++ Day17.4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {3, 4, 5, 6, 7};
+
+    int n1 = 5, n2 = 5;
+    int i, j;
+
+    printf("Common elements are");
+
+    for(i = 0; i < n1; i++)
+    {
+        for(j = 0; j < n2; j++)
+        {
+            if(arr1[i] == arr2[j])
+            {
+                printf("%d ", arr1[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
