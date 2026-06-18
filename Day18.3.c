03f785bc2ade581658926553d9cb4de84aa584e8
Sushant kumar <25121cn104@glbitm.ac.in>
2026-06-18 18:53:38 +0530

    Implement binary search in Day18.3.c

---
+++ Day18.3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60, 70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 50;
+    int low = 0, high = n - 1, mid;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+        {
+            printf("Element found at index %d", mid);
+            return 0;
+        }
+        else if (arr[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
